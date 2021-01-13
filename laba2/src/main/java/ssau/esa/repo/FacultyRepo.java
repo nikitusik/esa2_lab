ad7adcddc682514c3cf7e6f9ed43c7ad1239f94f
Nikita Yarychenko <52711686+nikitusik@users.noreply.github.com>
2021-01-13 19:01:18 +0400

    Add files via upload

---
+++ laba2/src/main/java/ssau/esa/repo/FacultyRepo.java
@@ -0,0 +1,7 @@
+package ssau.esa.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import ssau.esa.model.Faculty;
+
+public interface FacultyRepo extends CrudRepository<Faculty, Integer> {
+}
